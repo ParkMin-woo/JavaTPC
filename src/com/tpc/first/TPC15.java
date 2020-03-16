cb7589feecb41125303ba2632751d46d7d6c1bd7
Park Minwoo <lwmwpark3523@gmail.com>
2020-03-17 02:17:09 +0900

    branch test

---
+++ src/com/tpc/first/TPC15.java
@@ -0,0 +1,20 @@
+package com.tpc.first;
+
+import kr.bit.structure.MemberDTO;
+
+public class TPC15 {
+	public static void main(String[] args) {
+		MemberDTO member = new MemberDTO();
+		
+		member.setName("박민우");
+		System.out.println("name = " + member.getName());
+		member.setAge(29);
+		System.out.println("age = " + member.getAge());
+		member.setTel("010-4801-5126");
+		System.out.println("tel = " + member.getTel());
+		member.setAddr("서울시 송파구");
+		System.out.println("addr = " + member.getAddr());
+		
+		System.out.println("toString() method call : " + member.toString());
+	}
+}
