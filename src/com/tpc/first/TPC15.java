243c8ee09d944d016aac0db12b70a861229db429
Park Minwoo <lwmwpark3523@gmail.com>
2020-03-21 08:49:43 +0900

    TPC15 수정

--- src/com/tpc/first/TPC15.java
+++ src/com/tpc/first/TPC15.java
@@ -5,18 +5,27 @@ import kr.bit.structure.MemberDTO;
 public class TPC15 {
 	public static void main(String[] args) {
 		// getter , setter method 테스트중
-		MemberDTO member = new MemberDTO();
+		MemberDTO member1 = new MemberDTO();
 		
-		member.setName("박민우");
-		System.out.println("name = " + member.getName());
-		member.setAge(29);
-		System.out.println("age = " + member.getAge());
-		member.setTel("010-4801-5126");
-		System.out.println("tel = " + member.getTel());
-		member.setAddr("서울시 송파구");
-		System.out.println("addr = " + member.getAddr());
+		member1.setName("박민우");
+		System.out.println("name = " + member1.getName());
+		member1.setAge(29);
+		System.out.println("age = " + member1.getAge());
+		member1.setTel("010-4801-5126");
+		System.out.println("tel = " + member1.getTel());
+		member1.setAddr("서울시 송파구");
+		System.out.println("addr = " + member1.getAddr());
 		
-		System.out.println("toString() method call : " + member.toString());
+		System.out.println("toString() method call : " + member1.toString());
+		
+		MemberDTO member2 = new MemberDTO("강희애" , 28 , "010-4876-8395" , "서울 강남구");
+		System.out.println(member2.toString());
+		
+		// 객체참조변수명만 써도 자동으로 toString 메서드가 호출됨
+		// 아직은 왜그런지 모르겠음...;;;
+		System.out.println(member2);
+		System.out.println(member2.getClass());
+		System.out.println(member2.getClass().getName());
 	}
 }
  
