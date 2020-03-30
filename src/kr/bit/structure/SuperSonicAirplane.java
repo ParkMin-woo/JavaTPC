776d8bf94658463ab90c15bed30aa24eb938cdd6
Park Minwoo <lwmwpark3523@gmail.com>
2020-03-31 01:22:16 +0900

    Airplane Class로 실습해보는 하위 클래스에서의 상위 클래스의 메서드를 호출하는 실습.

---
+++ src/kr/bit/structure/SuperSonicAirplane.java
@@ -0,0 +1,18 @@
+package kr.bit.structure;
+
+public class SuperSonicAirplane extends Airplane {
+	public static final int NORMAL = 1;
+	public static final int SPECIAL = 2;
+	
+	public int flyMode = NORMAL;
+
+	@Override
+	public void fly() {
+		if(flyMode == SPECIAL) {
+			System.out.println("비행기가 초음속으로 날고 있는 중입니다.");
+		}
+		else {
+			super.fly();
+		}
+	}	
+}
