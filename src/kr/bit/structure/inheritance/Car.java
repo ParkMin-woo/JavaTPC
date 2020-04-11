f558e21809cd9488c1bb2c4149a8be141772e092
Park Minwoo <lwmwpark3523@gmail.com>
2020-04-11 22:19:29 +0900

    이것이 자바다 Car 에제 Tire 객체 배열로 관리

--- src/kr/bit/structure/inheritance/Car.java
+++ src/kr/bit/structure/inheritance/Car.java
@@ -1,44 +1,42 @@
 package kr.bit.structure.inheritance;
 
 public class Car {
+	/*
 	public Tire frontLeftTire = new Tire(6, "frontLeftTire");
 	public Tire frontRightTire = new Tire(10, "frontRightTire");
 	public Tire backLeftTire = new Tire(14, "backLeftTire");
 	public Tire backRightTire = new Tire(18, "backRightTire");
+	*/
 	
-	// public void run() {
+	// Tire 객체들을 배열로 관리
+	public Tire[] tires = new Tire[] {
+		new Tire(6, "frontLeftTire")
+		, new Tire(10, "frontRightTire")
+		, new Tire(14, "backLeftTire")
+		, new Tire(18, "backRightTire")
+	};
+	
 	public int run() {
-		System.out.println("자동차가 신나게 달리는 중입니다.");
-		
-		boolean tireTF1 = frontLeftTire.roll();
-		boolean tireTF2 = frontRightTire.roll();
-		boolean tireTF3 = backLeftTire.roll();
-		boolean tireTF4 = backRightTire.roll();
-		
 		/*
-		if(tireTF1 == false || tireTF2 == false || tireTF3 == false || tireTF4 == false) {
-			this.stop();
-		}
-		else {
-			
+		System.out.println("tires => " + tires);
+		System.out.println("tires.length => " + tires.length);
+		for(Tire temp : tires) {
+			System.out.println("temp => " + temp);
 		}
 		*/
+		System.out.println("자동차가 신나게 달리는 중입니다.");
+		/*
+		boolean tireTF1 = tires[0].roll();
+		boolean tireTF2 = tires[1].roll();
+		boolean tireTF3 = tires[2].roll();
+		boolean tireTF4 = tires[3].roll();
+		*/
 		
-		if(tireTF1 == false) {
-			stop();
-			return 1;
-		}
-		if(tireTF2 == false) {
-			stop();
-			return 2;
-		}
-		if(tireTF3 == false) {
-			stop();
-			return 3;
-		}
-		if(tireTF4 == false) {
-			stop();
-			return 4;
+		for(int i = 0 ; i < tires.length ; i++) {
+			if(tires[i].roll() == false) {
+				stop();
+				return (i+1);
+			}
 		}
 		return 0;
 	}
