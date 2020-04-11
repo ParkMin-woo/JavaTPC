a08fb53252220549ffc7a0af7b969ac519ff6fd7
Park Minwoo <lwmwpark3523@gmail.com>
2020-04-11 23:27:10 +0900

    이것이 자바다 Car 예제 source 수정

--- src/kr/bit/structure/inheritance/Car.java
+++ src/kr/bit/structure/inheritance/Car.java
@@ -1,13 +1,6 @@
 package kr.bit.structure.inheritance;
 
-public class Car {
-	/*
-	public Tire frontLeftTire = new Tire(6, "frontLeftTire");
-	public Tire frontRightTire = new Tire(10, "frontRightTire");
-	public Tire backLeftTire = new Tire(14, "backLeftTire");
-	public Tire backRightTire = new Tire(18, "backRightTire");
-	*/
-	
+public class Car {	
 	// Tire 객체들을 배열로 관리
 	public Tire[] tires = new Tire[] {
 		new Tire(6, "frontLeftTire")
@@ -17,23 +10,13 @@ public class Car {
 	};
 	
 	public int run() {
-		/*
-		System.out.println("tires => " + tires);
-		System.out.println("tires.length => " + tires.length);
-		for(Tire temp : tires) {
-			System.out.println("temp => " + temp);
-		}
-		*/
-		System.out.println("자동차가 신나게 달리는 중입니다.");
-		/*
-		boolean tireTF1 = tires[0].roll();
-		boolean tireTF2 = tires[1].roll();
-		boolean tireTF3 = tires[2].roll();
-		boolean tireTF4 = tires[3].roll();
-		*/
+		System.out.println("[자동차가 신나게 달리는 중입니다.]");
+		// System.out.println("tires.length => " + tires.length);
 		
 		for(int i = 0 ; i < tires.length ; i++) {
-			if(tires[i].roll() == false) {
+			boolean rollTF = tires[i].roll();
+			// if(tires[i].roll() == false) {
+			if(rollTF == false) {
 				stop();
 				return (i+1);
 			}
