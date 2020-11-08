fbd346d2d4e7592522da5af0b3d8183e6d197159
Park Minwoo <lwmwpark3523@gmail.com>
2020-11-08 23:14:25 +0900

    Object class로 다형성인수, 다형성배열 실습

---
+++ src/kr/bit/structure/inheritance/Temp02.java
@@ -0,0 +1,7 @@
+package kr.bit.structure.inheritance;
+
+public class Temp02 {
+	public void go() {
+		System.out.println("Temp02 gogo!");
+	}
+}
---
+++ src/kr/bit/structure/inheritance/Temp03.java
@@ -0,0 +1,7 @@
+package kr.bit.structure.inheritance;
+
+public class Temp03 {
+	public void go() {
+		System.out.println("Temp03 gogo!");
+	}
+}
