3fdcc1e90dddeb5cb1f1e7cff3ee8e38eb10c857
Park Minwoo <lwmwpark3523@gmail.com>
2020-11-08 17:13:53 +0900

    RemotementControl interface에 field 추가해서 실습

--- src/kr/bit/structure/implement/remocon/RemotementControl.java
+++ src/kr/bit/structure/implement/remocon/RemotementControl.java
@@ -1,6 +1,9 @@
 package kr.bit.structure.implement.remocon;
 
 public interface RemotementControl {
+	public final static int MAX_CHANNEL = 100;
+	public int MIN_CHANNEL = 1;
+	
 	public void chUp();
 	public void chDown();
 	public void internet();
--- src/kr/bit/structure/implement/remocon/TV.java
+++ src/kr/bit/structure/implement/remocon/TV.java
@@ -1,15 +1,34 @@
 package kr.bit.structure.implement.remocon;
 
 public class TV implements RemotementControl {
+	public int currentChannel = 80;
 
 	@Override
 	public void chUp() {
-		System.out.println("TV의 채널을 올립니다.");
+		if(currentChannel < RemotementControl.MAX_CHANNEL) {
+			currentChannel++;
+			System.out.println("TV의 채널을 올립니다.");
+			System.out.println("현재 채널 : " + currentChannel);
+		} else {
+			// currentChannel = 1;
+			currentChannel = RemotementControl.MIN_CHANNEL;
+			System.out.println("TV의 채널이 " + currentChannel + "로 초기화 되었습니다.");
+			System.out.println("현재 채널 : " + currentChannel);
+		}
 	}
 
 	@Override
 	public void chDown() {
-		System.out.println("TV의 채널을 내립니다.");
+		if(currentChannel > RemotementControl.MIN_CHANNEL) {
+			currentChannel--;
+			System.out.println("TV의 채널을 내립니다.");
+			System.out.println("현재 채널 : " + currentChannel);
+		} else {
+			// currentChannel = 100;
+			currentChannel = RemotementControl.MAX_CHANNEL;
+			System.out.println("TV의 채널이 " + currentChannel + "으로 초기화 되었습니다.");
+			System.out.println("현재 채널 : " + currentChannel);
+		}
 	}
 
 	@Override
